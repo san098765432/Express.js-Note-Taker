4143e8cff65d90d43bbe307411061aa7a65a6a94
Sanyiah Zulfiqar <sanyiahzulfiqar1996@gmail.com>
2022-04-30 10:50:04 +0100

    added server.js and connected to port

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-// to write 
+const express = require ('express') 
+const PORT = process.env.PORT || 8080 
+const app = express()   
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.listen(PORT, () => console.log(`App listening on port http://localhost:${PORT}`)); 
