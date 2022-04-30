0b70555ecd14e3331a0d50a0b84663a0f0cb9a86
Sanyiah Zulfiqar <sanyiahzulfiqar1996@gmail.com>
2022-04-30 11:34:05 +0100

    added post route

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,5 +1,6 @@
 const router = require ('express').Router()
 const fs = require ('fs')
+const uniqid = require ('uniqid')
 
 router.get('/api/notes',(req, res)=> {
     fs.readFile('db/db.json', 'utf8', (err,data)=>{
@@ -7,5 +8,19 @@ router.get('/api/notes',(req, res)=> {
         return res.json(JSON.parse(data))
     })
 })
+
+router.post ('/api/notes', (req, res)=> {
+    fs.readFile('db/db.json', 'utf8', (err,data)=>{
+        if(err) throw err
+        const db = JSON.parse(data)
+        req.body.id = uniqid()
+        db.push(req.body)
+        fs.writeFile('db/db.json',JSON.stringify(db) , (err)=> {
+            if (err) throw err 
+            res.json (db)
+        })
+    })
+})
+
 
 module.exports = router
